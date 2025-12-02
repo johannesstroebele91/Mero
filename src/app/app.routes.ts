bc94cb9b2a8d1b389cee831af1fc825b06f0266f
JohannesStroebele <johannes.stroebele@bridging-it.de>
2025-12-03 00:41:22 +0100

    Add routing and improve app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./components/login/login').then(m => m.Login)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./components/register/register').then(m => m.Register)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./components/home/home').then(m => m.Home),
+    canActivate: [authGuard]
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <app-header></app-header>
+
+    <div class="app-content-root">
+      <router-outlet></router-outlet>
+    </div>
+  `,
+  styles: [`
+  .app-content-root { display:block; min-height:100vh; padding: 40px }`
+  ]
 })
-export class App {
-  protected title = 'Mero';
-}
+export class App {}
